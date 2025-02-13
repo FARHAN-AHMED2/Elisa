cd4fa2a5144c4869444d72f8eb37a7fd26b03ac5
akash0044373 <akashkhan18430@gmail.com>
2025-02-13 18:26:55 +0600

    Update prefix.js

--- scripts/cmds/prefix.js
+++ scripts/cmds/prefix.js
@@ -99,7 +99,7 @@ module.exports = {
     const name = data.name;
     const xyrene = {
       body: `🈷\x20\x20\x20\x20\x20\x20\x20\x20\x20${name}\x20\x20\x20\x20\x20\x20\x20\x20\x20🈷` + getLang("myPrefix", global.GoatBot.config.prefix, utils.getPrefix(event.threadID)),
-      attachment: await global.utils.getStreamFromURL("https://i.imgur.com/ikBzCwh.jpeg")
+      attachment: await global.utils.getStreamFromURL("https://i.imgur.com/LCJ9eFx.mp4")
         };
     if (event.body && event.body.toLowerCase() === "prefix")
       return () => {
