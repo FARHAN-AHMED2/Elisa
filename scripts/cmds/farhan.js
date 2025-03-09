f3c0eeabf0d7d0ddb13284f85fdf10ce205c9abd
𝐅𝐀𝐑𝐇𝐀𝐍 𝐀𝐇𝐌𝐄𝐃 <akashkhan18430@gmail.com>
2025-03-09 10:10:28 +0600

    Update farhan.js

--- scripts/cmds/farhan.js
+++ scripts/cmds/farhan.js
@@ -1,22 +1,22 @@
 module.exports = {
- config: {
-	 name: "bayjid",
-	 version: "1.0",
-	 author: "AceGun",
-	 countDown: 5,
-	 role: 0,
-	 shortDescription: "no prefix",
-	 longDescription: "no prefix",
-	 category: "no prefix",
- },
+config: {
+name: "farhan",
+version: "1.0",
+author: "NTKhang | edited by Farhan",
+countDown: 5,
+role: 0,
+shortDescription: "no prefix",
+longDescription: "no prefix",
+category: "no prefix",
+},
 
- onStart: async function(){}, 
- onChat: async function({ event, message, getLang }) {
- if (event.body && event.body.toLowerCase() === "farhan") {
- return message.reply({
- body: " 「❥︎----ღ᭄_ᴬˢˢᴬᴸᴬᴹᴼᴸᴬᴵᴷᵁᴹ ..\n❥︎----ღ᭄_  ᴮᴿᴼᵀᴴᴱᴿ❞࿐.🌴.\n❥ ғᴀʀʜᴀɴ ᴀʜᴍᴇᴅ\n\n𝗕𝗢𝗧 𝗢𝗪𝗡𝗘𝗥\n𝐅𝐀𝐑𝐇𝐀𝐍 𝐀𝐇𝐌𝐄𝐃」",
- attachment: await global.utils.getStreamFromURL("https://i.imgur.com/XlyIOMR.mp4")
- });
- }
- }
+onStart: async function(){}, 
+onChat: async function({ event, message, getLang }) {
+if (event.body && event.body.toLowerCase() === "farhan") {
+return message.reply({
+body: " ──────────◊\n  ❥➳ 𝗔𝘀𝘀𝗮𝗹𝗮𝗺𝘂𝗮𝗹𝗮𝗶𝗸𝘂𝗺 ★★  \n\n  ❥➳ 𝗥𝗮𝗺𝗮𝗱𝗮𝗻 𝗞𝗮𝗿𝗲𝗲𝗺 ★★ \n\n  𝗕𝗢𝗧 𝗢𝗪𝗡𝗘𝗥 \n 𝐅𝐀𝐑𝐇𝐀𝐍 𝐀𝐇𝐌𝐄𝐃 ✦✼ ",
+attachment: await global.utils.getStreamFromURL("https://i.imgur.com/mMH0N1S.mp4")
+});
+}
+}
 }
